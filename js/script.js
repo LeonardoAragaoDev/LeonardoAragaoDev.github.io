abb7ef68ad90cd8c586456594b922e3815e265c5
Leonardo Aragão <leonardo.lca@isgh.org.br>
2023-01-20 16:59:21 -0300

    add lista de objetos com info dos projetos

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
+var listaObjetoProjetos = [
+    {
+        nome: "Projeto 1",
+        tecnologias: ["HTML", "CSS", "JS"],
+        descricao: "Descrição do projeto 1",
+        link: "Link do projeto 1"
+    },
+
+    {
+        nome: "Projeto 2",
+        tecnologias: ["HTML", "CSS"],
+        descricao: "Descrição do projeto 2",
+        link: "Link do projeto 2"
+    },
+
+    {
+        nome: "Projeto 3",
+        tecnologias: ["HTML", "CSS", "JS", "API"],
+        descricao: "Descrição do projeto 3",
+        link: "Link do projeto 3"
+    }
+]
+
 var listaProjetos = document.getElementById("listaProjetos")
 var paginacaoListaProjetos = document.getElementById("paginacaoListaProjetos")
 
-for (let i = 1; i < 10; i++) {
+listaObjetoProjetos.forEach(element => {
     listaProjetos.innerHTML += `
     <div class="card bg-dark bg-gradient" style="width: 18rem;">
     <img src="https://www.flowup.me/blog/wp-content/uploads/2019/09/gest%C3%A3o-de-projetos-01.png"
         class="card-img-top" alt="...">
     <div class="card-body">
-        <span class="badge text-info">HTML</span>
-        <span class="badge text-info">CSS</span>
-        <span class="badge text-info">JS</span>
-        <h5 class="card-title mt-2">Projeto ${i}</h5>
-        <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas
-            quasi dicta nesciunt ad vitae dolorum laudantium doloribus consectetur omnis, nam
-            nihil
-            quas ipsam aut pariatur, fuga ex ut consequuntur error?</p>
-        <a href="#" class="btn btn-primary">Conferir</a>
+        <span class="badge text-info">${element.tecnologias}</span>
+        <h5 class="card-title mt-2">${element.nome}</h5>
+        <p class="card-text">${element.descricao}</p>
+        <a href="${element.link}" class="btn btn-primary">Conferir</a>
     </div>
     </div>
     `
-}
+    // <span class="badge text-info">HTML</span>
+    //     <span class="badge text-info">CSS</span>
+    //     <span class="badge text-info">JS</span>
+})
 
 paginacaoListaProjetos.innerHTML += `
 <nav aria-label="Paginacao de projetos">
